bfee173943d9437463187b8bd404669bd83b960e
imostima227 <1172842275@qq.com>
2025-10-09 12:55:17 +0800

    update

--- codes/JAVA/01_array/src/ArraySort.java
+++ codes/JAVA/01_array/src/ArraySort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class ArraySort {
     public static void main(String[] args) {
         int[] nums = {1,2,3,7,5,6,10,3};
-        insertionSort(nums);
+        mergeSort(nums);
         System.out.println(Arrays.toString(nums));
     }
 
@@ -56,5 +56,57 @@ public class ArraySort {
             }
             nums[idx] = key;
         }
+    }
+
+    // 其实不太会
+    public static void shellSort(int[] nums){
+        int len = nums.length;
+        int gap = len / 2;
+        while (gap > 0) {
+            for (int i = gap; i < len; i++) {
+                int tmp = nums[i], j;
+                for (j = i; j >= gap && tmp < nums[j - gap]; j -= gap) {
+                    nums[j] = nums[j - gap];
+                }
+                nums[j] = tmp;
+            }
+            gap /= 2;
+        }
+    }
+
+    public static void mergeSort(int[] nums){
+        if (nums == null || nums.length < 2) {
+            return;
+        }
+        int[] ans = new int[nums.length];
+        sort(nums, 0, nums.length - 1, ans);
+    }
+
+    public static void sort(int[] nums, int left, int right, int[] ans){
+        if (left >= right) {
+            return;
+        }
+        int mid = (left + right) / 2;
+        sort(nums, left, mid, ans);
+        sort(nums, mid + 1, right, ans);
+        merge(nums, left, mid, right, ans);
+    }
+
+    public static void merge(int[] nums, int left, int mid, int right, int[] ans) {
+        System.arraycopy(nums, left, ans, left, right - left + 1);
+        int i = left;
+        int j = mid + 1;
+        int k =  left;
+        while (i <= mid && j <= right) {
+            nums[k++] = (ans[i] <= ans[j]) ? ans[i++] : ans[j++];
+        }
+
+        while (i <= mid) {
+            nums[k++] = ans[i++];
+        }
+//        while (j <= right) {
+//            nums[k++] = ans[j++];
+//        }
+
     }
 }
