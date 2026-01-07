fc8bc174217fbf98212fb3546b289725b83e3fd0
imostima227 <1172842275@qq.com>
2026-01-07 09:49:07 +0800

    update

--- codes/javascript/03_stack_queue_hash_table/stack_basic.js
+++ codes/javascript/03_stack_queue_hash_table/stack_basic.js
@@ -107,11 +107,87 @@ class MinStack{
  * var param_4 = obj.getMin()
  */
 
+// 逆波兰数求解
+const evalRPN = function(tokens) {
+    const stack = [];
+    
+    for (const token of tokens) {
+        if (token === '+' || token === '-' || token === '*' || token === '/'){
+            const num2 = stack.pop();
+            const num1 = stack.pop();
+            
+            let res = 0;
+            switch (token) {
+                case '+':
+                    res = num1 + num2;
+                    break;
+                case '-':
+                    res = num1 - num2;
+                    break;
+                case '*':
+                    res = num1 * num2;
+                    break;
+                case '/':
+                    res = Math.trunc(num1 / num2);
+                    break;
+            }
+            stack.push(res);
+        } else {
+            stack.push(Number(token))
+        }
+    }
+    return stack.at(-1);
+};
+
+// 字符串解码
+const decodeString = function(s) {
+    const stack = [];
+    let curNum = 0;
+    let curStr = '';
+    for(const c of s){
+        if (c >= '0' && c <= '9') {
+            curNum = curNum * 10 + Number(c); // 处理多位数
+        } 
+        else if (c == '[') {
+            // 入栈
+            stack.push([curNum, curStr]);
+            curNum = 0;
+            curStr = '';
+        }
+        else if (c == ']') {
+            const [preNum, preStr] = stack.pop();
+            curStr = preStr + curStr.repeat(preNum); 
+        }
+        else // 普通字符
+            curStr += c;
+    }
+    return curStr;
+};
+
+// 验证栈队列
+const validateStackSequences = function(pushed, popped) {
+    const stack = [];
+    let pop_i = 0;
+    for (const n of pushed) {
+        stack.push(n);
+        while (stack.length && stack.at(-1) === popped[pop_i]) {
+            stack.pop();
+            pop_i++;
+        }
+    }
+
+    return stack.length === 0;
+};
 
 
 function main(){
-    const s = '3+2*2';
-    console.log(calculate(s));
+    // const s = '3+2*2';
+    // console.log(calculate(s));
+    // const tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"];
+    // console.log(evalRPN(tokens))
+    const pushed = [1,2,3,4,5];
+    const popped = [4,5,3,2,1];
+    validateStackSequences(pushed, popped);
 }
 
 main()
